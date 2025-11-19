dbc93ef41fcc8d791531f33d4102b05d8eceb126
PP000777 <bia.silva007@hotmail.com>
2025-11-19 12:08:39 -0300

    alteração no serv, que nao tava funcionando

--- server.js
+++ server.js
@@ -1,70 +1,68 @@
-import express, { response } from 'express'
+import express from 'express'
+import cors from 'cors'
 import pg from 'pg'
 
 const { Pool } = pg
 
-const server = express();
+const server = express()
 server.use(express.json())
+server.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}))
 
 const sql = new Pool({
     host: 'localhost',
-    user: 'local',
+    user: 'local', 
     password: '12345',
     database: 'teste',
     port: 5432
 })
 
-
 server.get('/users', async (req, res) => {
     try {
         const resultado = await sql.query('SELECT * FROM users')
-
-        return res.json({ resuts: response.rows, ok: true })
+        return res.json({ results: resultado.rows, ok: true })
     } catch (error) {
         res.status(500).json({ message: error, ok: false })
     }
-
-
 })
 
 server.post('/users', async (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    const profile = req.body.profile;
+    const { name, email, password, profile } = req.body
+
     try {
-        const response = await sql.query('INSERT INTO users (name, email, password, profile) VALUES ($1, $2, $3, $4)', [name, email, password, profile])
-        return res.send('usuario cadastrado com sucesso!!')
+        await sql.query(
+            'INSERT INTO users (name, email, password, profile) VALUES ($1, $2, $3, $4)',
+            [name, email, password, profile]
+        )
+        return res.send('Usuário cadastrado com sucesso!!')
     }
     catch (error) {
         res.status(500).json({ message: error, ok: false })
+    }
+})
 
-    }})
+server.post('/login', async (req, res) => {
+    const { email, password } = req.body
 
-    server.use(cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    }))
+    try {
+        const resposta = await sql.query(
+            'SELECT * FROM users WHERE email = $1 AND password = $2',
+            [email, password]
+        )
 
-    server.post('/login', async (req, res) => {
-        const name = req.body.name;
-        const email = req.body.email;
-        const password = req.body.password;
-        const profile = req.body.profile;
-
-        try {
-            const response = await sql.query('SELECT * FROM users WHERE email = $1 AND password = $2', [email, password])
-            if (response.rows.length > 0) {
-                return res.json({ message: 'Login bem sucedido!', ok: true })
-            } else {
-                return res.status(401).json({ message: 'Credenciais inválidas', ok: false })
-            }
+        if (resposta.rows.length > 0) {
+            return res.json({ message: 'Login bem sucedido!', ok: true })
+        } else {
+            return res.status(401).json({ message: 'Credenciais inválidas', ok: false })
         }
-        catch (error) {
-            res.status(500).json({ message: error, ok: false })
-    
-        }})
+    }
+    catch (error) {
+        res.status(500).json({ message: error, ok: false })
+    }
+})
 
 server.listen(3000, () => {
-    console.log('Ta rodando!: http://localhost:3000/')
+    console.log('Rodando: http://localhost:3000/')
 })
